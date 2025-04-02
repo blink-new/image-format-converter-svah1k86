fb86f7bddd77fadba43260b4c8e21e8f9b0bd2c2
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-02 19:04:20 +0000

    Fix theme toggle component imports

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,7 @@
 
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "@/lib/themes"
+import { useTheme } from "@/components/theme-provider"
 
 import { Button } from "@/components/ui/button"
 import {
